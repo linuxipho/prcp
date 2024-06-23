b5db0647e7c986790b59f03e8423a0be678f1f83
Rémi de Chazelles <remidechazelles@gmail.com>
2024-06-24 00:06:25 +0200

    feat: première version des doctypes <Membre> et <Journal>

---
+++ prcp/prcp/doctype/membre/membre.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Rémi de Chazelles and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Membre", {
+// 	refresh(frm) {
+
+// 	},
+// });
